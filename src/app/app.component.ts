796385e16b965a09799f1d52ccff0a8159797523
Ana Beatriz <anabe4af.almeida@gmail.com>
2020-10-01 14:07:20 -0300

    Primeiro commit dos arquivos, já colocados no angular e utilizado dois componentes, feita lista de cursos e lista de professores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ElementRef, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'projeto-universidade-ensino-mais';
+
+  constructor(){}
+
+  ngOnInit(){}
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListaCursoComponent } from './lista-curso/lista-curso.component';
+import { ListaProfessoresComponent } from './lista-professores/lista-professores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaCursoComponent,
+    ListaProfessoresComponent
   ],
   imports: [
     BrowserModule
